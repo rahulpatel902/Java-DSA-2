80d38da24dcf7a55fc0eba2c75aa1c6c4a9701f7
Rahul Patel <mp902785@gmail.com>
2022-10-25 14:14:04 +0530

    Updated L3 : Variables and Datatypes

--- 01-Java-Basics/Codes/L3_Variables_and_Datatypes/src/_01_Variables_Output.java
+++ 01-Java-Basics/Codes/L3_Variables_and_Datatypes/src/_01_Variables_Output.java
@@ -2,16 +2,15 @@ public class _01_Variables_Output {
 
     public static void main(String[] args){
 
-     //   System.out.println(args[0] + args[1]);
+        //   System.out.println(args[0] + args[1]);
 
-  // Variable_name = money, variable_value = 2000, variable type = integer
+        // Variable_name = money, variable_value = 2000, variable type = integer
 
         int money = 2000;
         System.out.println("Money : " + money);
 
         money = 5000; // here money value is changed
         System.out.println("Money : " + money);
-
-
     }
 }
+
---
+++ 01-Java-Basics/Codes/L3_Variables_and_Datatypes/src/_02_Datatypes.java
@@ -0,0 +1,27 @@
+public class _02_Datatypes {
+    public static void main(String[] args) {
+
+        boolean flag = true;
+
+        byte a = 127;
+        short b = 32767;
+        int c = 999999999;
+        long d = 999999999999999999L;
+
+        float e = 880.7692F;
+        double f = 7878.835354;
+
+        char ch = 'p';
+
+        System.out.println("The Boolean is " + flag);
+        System.out.println("The Byte  is " + a);
+        System.out.println("The Short  is " + b);
+        System.out.println("The Int  is " + c);
+        System.out.println("The Long  is " + d);
+        System.out.println("The Float  is " + e);
+        System.out.println("The Double  is " + f);
+        System.out.println("The Char  is " + ch);
+
+
+    }
+}
