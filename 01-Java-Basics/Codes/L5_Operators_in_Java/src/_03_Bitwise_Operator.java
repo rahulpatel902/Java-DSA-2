283aa590c64735867c9ccb3c03d8a9f10a4cbdc3
Rahul Patel <mp902785@gmail.com>
2022-11-17 19:06:12 +0530

    Updated L5 : Operators -> Topic Completed : Bitwise Operator

---
+++ 01-Java-Basics/Codes/L5_Operators_in_Java/src/_03_Bitwise_Operator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class _03_Bitwise_Operator {
+    public static void main(String[] args) {
+            /*Bitwise Operators*/
+        // [ | ] => Bitwise OR
+        // [ & ] => Bitwise AND
+        // [ ^ ] => Bitwise Exclusive OR / XOR
+        // [ << ] => Bitwise Left Shift
+        // [ >> ] => Bitwise Right Shift
+        // [ >>> ] => Unsigned Right Shift
+        // [ ~ ] => Bitwise Complement
+
+        //Test Code for bitwise operators
+/*        int p = 9, q = 10;
+        System.out.println(p | q); //11 = 1001 | 1010 = 1011
+        System.out.println(p & q); // 8 = 1001 & 1010 = 1000
+        System.out.println(p ^ q); // 3 = 1001 ^ 1010 = 0011
+
+
+        System.out.println(p << 1); // 18 = 10010
+        System.out.println(p << 2); // 36 = 100100
+        System.out.println(q >> 1); // 1010 = 0101 = 5
+        System.out.println(q >> 2); // 1010 = 0010 = 2*/
+
+        //MCQ
+
+
+
+
+
+    }
+}
