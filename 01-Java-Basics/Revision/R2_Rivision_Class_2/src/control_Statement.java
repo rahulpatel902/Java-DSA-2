2874b48c06358261138ee1796938d47fbbca2bfa
Rahul Patel <mp902785@gmail.com>
2022-12-10 16:45:50 +0530

    Completed R2 : Java Rivision Class 2

---
+++ 01-Java-Basics/Revision/R2_Rivision_Class_2/src/control_Statement.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class control_Statement {
+    public static void main(String[] args) {
+                /*  IF-Else */
+
+        //Q1:Take positive integer input and tell if it
+        //is even or odd
+/*
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter a Positive Integer");
+            int n = input.nextInt();
+
+            if(n%2==0)
+                System.out.println(n + " is a Even Number");
+            else
+                System.out.println(n + " is a Odd Number");
+        }
+*/
+        
+
+
+    }
+}
